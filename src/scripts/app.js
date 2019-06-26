6db1e01d253f4f06e7d9fd3147ff36c90957ff73
Piotr Dzienis <dojlidy.zubr@gmail.com>
2019-06-26 15:15:08 +0200

    set hover on whole box, change color on headline and btn, and link in app.js

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,10 @@
 console.log('hello');
+
+var postBox = document.getElementsByClassName('post-box');
+
+for (var i = 0; i < postBox.length; i++) {
+  postBox[i].addEventListener('click', function (event) {
+    event.preventDefault();
+    window.location = this.href;
+  });
+}
