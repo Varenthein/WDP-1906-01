e78682c6a554ecf270c44deebe10a8d3b9a7670b
Piotr Dzienis <dojlidy.zubr@gmail.com>
2019-07-01 16:50:06 +0200

    add js function to make href in feature-box class

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,10 @@
 console.log('hello');
+
+var featureBox = document.getElementsByClassName('feature-box');
+
+for (var i = 0; i < featureBox.length; i++) {
+  featureBox[i].addEventListener('click', function (event) {
+    event.preventDefault();
+    window.location = this.href;
+  });
+}
