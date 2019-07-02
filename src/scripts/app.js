547e5db36cf000da6e1852db32e8426aa6a35fd3
Piotr Dzienis <dojlidy.zubr@gmail.com>
2019-07-02 16:21:48 +0200

    put post content in <a> tag

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,10 +1,1 @@
 console.log('hello');
-
-var postBox = document.getElementsByClassName('post-box');
-
-for (var i = 0; i < postBox.length; i++) {
-  postBox[i].addEventListener('click', function (event) {
-    event.preventDefault();
-    window.location = this.href;
-  });
-}
