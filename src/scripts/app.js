1b6b0e019701d610e466eeeba3e8337455972931
Piotr Dzienis <dojlidy.zubr@gmail.com>
2019-07-02 16:08:18 +0200

    del js function

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,10 +1,1 @@
 console.log('hello');
-
-var featureBox = document.getElementsByClassName('feature-box');
-
-for (var i = 0; i < featureBox.length; i++) {
-  featureBox[i].addEventListener('click', function (event) {
-    event.preventDefault();
-    window.location = this.href;
-  });
-}
