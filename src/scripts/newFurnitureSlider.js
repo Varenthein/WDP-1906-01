dac8738369a5ed0d0e92f0b657ff6a2fafc40fdd
Łukasz Nolka <nolkalukasz@gmail.com>
2019-07-15 19:28:28 +0200

    Add sliders for all tabs

--- src/scripts/newFurnitureSlider.js
+++ src/scripts/newFurnitureSlider.js
@@ -1,5 +1,5 @@
-var slider = tns({
-  container: '.my-slider',
+let bedSlider = tns({
+  container: '.bed-slider',
   items: 4,
   slideBy: 'page',
   speed: 600,
@@ -22,3 +22,104 @@ var slider = tns({
     }
   }
 });
+
+let chairSlider = tns({
+  container: '.chair-slider',
+  items: 4,
+  slideBy: 'page',
+  speed: 600,
+  controls: false,
+  autoplay: false,
+  autoplayButtonOutput: false,
+  preventScrollOnTouch: 'auto',
+  responsive: {
+    0: {
+      items: 1
+    },
+    576: {
+      items: 2
+    },
+    768: {
+      items: 3
+    },
+    992: {
+      items: 4
+    }
+  }
+});
+
+let sofaSlider = tns({
+  container: '.sofa-slider',
+  items: 4,
+  slideBy: 'page',
+  speed: 600,
+  controls: false,
+  autoplay: false,
+  autoplayButtonOutput: false,
+  preventScrollOnTouch: 'auto',
+  responsive: {
+    0: {
+      items: 1
+    },
+    576: {
+      items: 2
+    },
+    768: {
+      items: 3
+    },
+    992: {
+      items: 4
+    }
+  }
+});
+
+let tableSlider = tns({
+  container: '.table-slider',
+  items: 4,
+  slideBy: 'page',
+  speed: 600,
+  controls: false,
+  autoplay: false,
+  autoplayButtonOutput: false,
+  preventScrollOnTouch: 'auto',
+  responsive: {
+    0: {
+      items: 1
+    },
+    576: {
+      items: 2
+    },
+    768: {
+      items: 3
+    },
+    992: {
+      items: 4
+    }
+  }
+});
+
+let diningSlider = tns({
+  container: '.dining-slider',
+  items: 4,
+  slideBy: 'page',
+  speed: 600,
+  controls: false,
+  autoplay: false,
+  autoplayButtonOutput: false,
+  preventScrollOnTouch: 'auto',
+  responsive: {
+    0: {
+      items: 1
+    },
+    576: {
+      items: 2
+    },
+    768: {
+      items: 3
+    },
+    992: {
+      items: 4
+    }
+  }
+});
+
